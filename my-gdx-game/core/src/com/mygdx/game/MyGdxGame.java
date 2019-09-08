ce7fd98e08fdec399da1005b14a8781a0f21a782
rgunczer <rgunczer@gmail.com>
2019-09-08 20:43:23 +0200

    simple image bouncing

--- my-gdx-game/core/src/com/mygdx/game/MyGdxGame.java
+++ my-gdx-game/core/src/com/mygdx/game/MyGdxGame.java
@@ -9,19 +9,55 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	float x;
+	float y;
+	float xStep;
+	float yStep;
+	float speed;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		x = 0f;
+		y = 0f;
+		speed = 600f;
+		xStep = speed;
+		yStep = speed;
 	}
 
 	@Override
 	public void render () {
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		x += xStep * deltaTime;
+		y += yStep * deltaTime;
+
+		if (x < 0f) {
+			x = 0f;
+			xStep = speed;
+		}
+
+		if (x + img.getWidth() > Gdx.graphics.getWidth()) {
+			x = Gdx.graphics.getWidth() - img.getWidth();
+			xStep = -speed;
+		}
+
+		if (y < 0f) {
+			y = 0f;
+			yStep = speed;
+		}
+
+		if (y + img.getHeight() > Gdx.graphics.getHeight()) {
+			y = Gdx.graphics.getHeight() - img.getHeight();
+			yStep = -speed;
+		}
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	

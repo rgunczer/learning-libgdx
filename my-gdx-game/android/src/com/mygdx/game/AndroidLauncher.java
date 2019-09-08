d923cec51e2930b981bbeb9557ffeed579863ed4
rgunczer <rgunczer@gmail.com>
2019-09-08 21:12:14 +0200

    bounce in ortho camera space

--- my-gdx-game/android/src/com/mygdx/game/AndroidLauncher.java
+++ my-gdx-game/android/src/com/mygdx/game/AndroidLauncher.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.MyGdxGameSimpleBounceRealPixelSize;
+import com.mygdx.game.MyGdxGameBounceUsingOrthoCameraNormCoords;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGameSimpleBounceRealPixelSize(), config);
+//		initialize(new MyGdxGameSimpleBounceRealPixelSize(), config);
+		initialize(new MyGdxGameBounceUsingOrthoCameraNormCoords(), config);
 	}
 }
---
+++ my-gdx-game/core/src/com/mygdx/game/MyGdxGameBounceUsingOrthoCameraNormCoords.java
@@ -0,0 +1,91 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MyGdxGameBounceUsingOrthoCameraNormCoords extends ApplicationAdapter {
+
+    SpriteBatch batch;
+    Texture img;
+
+    Sprite sprite;
+    OrthographicCamera camera;
+
+    float speed = 0.4f;
+    float x;
+    float y;
+    float xStep = speed;
+    float yStep = speed;
+    float ch = 1.0f;
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        ch = h / w;
+
+        camera = new OrthographicCamera(1, h / w);
+
+        TextureRegion region = new TextureRegion(img, 0, 0, img.getWidth(), img.getHeight());
+        sprite = new Sprite(region);
+        sprite.setSize(0.2f, 0.2f * sprite.getHeight() / sprite.getWidth());
+
+        x = -sprite.getWidth() / 2f;
+        y = -sprite.getHeight() / 2f;
+
+        sprite.setOrigin(sprite.getWidth() / 2f, sprite.getHeight() / 2f);
+    }
+
+    @Override
+    public void render () {
+        float deltaTime = Gdx.graphics.getDeltaTime();
+
+        x += xStep * deltaTime;
+        if (x + sprite.getWidth() > 0.5f) {
+            x = 0.5f - sprite.getWidth();
+            xStep = -speed;
+        }
+
+        if (x < -0.5f) {
+            x = -0.5f;
+            xStep = speed;
+        }
+
+        y += yStep * deltaTime;
+        if (y + sprite.getHeight() > ch/2.0f) {
+            y = (ch / 2.0f) - sprite.getHeight();
+            yStep = -speed;
+        }
+
+        if (y < -ch/2.0f) {
+            y = -ch/2.0f;
+            yStep = speed;
+        }
+
+        Gdx.gl.glClearColor(0, 1, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        sprite.setPosition(x, y);
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        img.dispose();
+    }
+
+}

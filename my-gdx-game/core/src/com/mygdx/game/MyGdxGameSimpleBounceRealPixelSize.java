dfb6d18dd88a76b5e45628dada074b71795df29c
rgunczer <rgunczer@gmail.com>
2019-09-08 20:50:10 +0200

    renames

--- my-gdx-game/core/src/com/mygdx/game/MyGdxGameSimpleBounceRealPixelSize.java
+++ my-gdx-game/core/src/com/mygdx/game/MyGdxGameSimpleBounceRealPixelSize.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGameSimpleBounceRealPixelSize extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 
@@ -54,7 +54,7 @@ public class MyGdxGame extends ApplicationAdapter {
 			yStep = -speed;
 		}
 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, x, y);
